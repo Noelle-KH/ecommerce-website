01e65590d3115faaf1426a91bbb66672a52036c1
Noelle-KH <noelletang86@gmail.com>
2023-07-13 23:32:18 +0800

    feat: add header and footer view

--- frontend/src/components/Header/PageHeader.vue
+++ frontend/src/components/Header/PageHeader.vue
@@ -1,5 +1,56 @@
-<script setup></script>
+<script setup>
+import SearchIcon from '../icons/SearchIcon.vue'
+import CartIcon from '../icons/CartIcon.vue'
+
+const emits = defineEmits(['openModal'])
+
+const showLoginModal = () => {
+	emits('openModal')
+}
+</script>
 
 <template>
-	<header>Header</header>
+	<header class="px-10 py-5">
+		<nav class="flex flex-wrap items-center justify-between">
+			<div>
+				<img class="w-24" src="src/assets/logo.png" alt="logo" />
+			</div>
+			<div class="flex flex-1 flex-col items-center">
+				<div class="flex">
+					<input
+						class="rounded-l-md border border-stone-600 px-2 sm:w-72"
+						type="text"
+						placeholder="搜尋關鍵字"
+					/>
+					<button
+						class="flex h-10 items-center rounded-r-md border border-stone-600 bg-orange-400 px-2 hover:bg-orange-300"
+					>
+						<SearchIcon class="w-8" />
+					</button>
+				</div>
+				<p class="mt-2 text-sm">
+					熱門關鍵字：
+					<button
+						class="cursor-pointer rounded-full border border-stone-300 bg-stone-300 px-3 py-1 hover:bg-stone-200"
+					>
+						狗狗罐頭
+					</button>
+					<button
+						class="ml-1 cursor-pointer rounded-full border border-stone-300 bg-stone-300 px-3 py-1 hover:bg-stone-200"
+					>
+						貓貓罐頭
+					</button>
+				</p>
+			</div>
+			<div class="flex gap-5">
+				<CartIcon class="w-8 cursor-pointer hover:text-stone-600" />
+				<button
+					class="rounded-lg bg-orange-400 px-8 hover:bg-orange-300"
+					@click="showLoginModal"
+				>
+					登入
+				</button>
+			</div>
+		</nav>
+	</header>
 </template>
