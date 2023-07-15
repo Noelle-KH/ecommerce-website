a3fe4ab87435ea8ce63b0ec573911d215fb41e8e
Noelle-KH <noelletang86@gmail.com>
2023-07-15 15:12:42 +0800

    feat: add product seed, update user and cart seed

--- backend/src/prisma/seed.js
+++ backend/src/prisma/seed.js
@@ -1,17 +1,30 @@
 const bcrypt = require('bcryptjs')
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
+const produceSeed = require('./seeds/product-seed.json')
 
 async function main () {
-  await prisma.user.createMany({
-    data: [
-      { account: 'buyer001', password: bcrypt.hashSync('titaner', 10) },
-      {
-        account: 'seller001',
+  await prisma.user.create({
+    data: {
+      account: 'seller001',
+      password: bcrypt.hashSync('titaner', 10),
+      role: 'seller'
+    }
+  })
+
+  await prisma.product.createMany({
+    data: produceSeed
+  })
+
+  await prisma.$transaction(async (tx) => {
+    const buyer = await tx.user.create({
+      data: {
+        account: 'buyer001',
         password: bcrypt.hashSync('titaner', 10),
-        role: 'seller'
+        role: 'buyer'
       }
-    ]
+    })
+    await tx.cart.create({ data: { buyerId: buyer.id } })
   })
 }
 
