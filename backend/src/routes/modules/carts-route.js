c6edb8865207d8b7391e0c0b318a1e120f1b34af
Noelle-KH <noelletang86@gmail.com>
2023-07-16 23:19:30 +0800

    doc: update api document

--- backend/src/routes/modules/carts-route.js
+++ backend/src/routes/modules/carts-route.js
@@ -1,16 +1,15 @@
 const router = require('express').Router()
 const cartController = require('../../controllers/cart-controller')
 
-router.post('/:productId', cartController.postCartItem)
-
 router
-  .route('/:id')
+  .route('/:cartItemId')
   .patch(cartController.updateCartItem)
   .delete(cartController.deleteCartItem)
 
 router
   .route('/')
   .get(cartController.getCartItems)
+  .post(cartController.postCartItem)
   .patch(cartController.updateCartStatus)
 
 module.exports = router
